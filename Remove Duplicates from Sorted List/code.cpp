fb74f18ccab49894b90c12cad3d7d98613a6018c
Dev_Saiyam <70647163+saiyamdubey@users.noreply.github.com>
2024-02-20 23:27:11 +0530

    Add files via upload

---
+++ Remove Duplicates from Sorted List/code.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL|| head->next==NULL){
+            return head;
+        }
+        ListNode *temp=head;
+        while(temp && temp->next){
+            if(temp->val==temp->next->val){
+                temp->next=temp->next->next;
+            }
+            else{
+                temp=temp->next;
+            }
+        }
+        return head;
+    }
+};
+
